ded5eca3d37824677c5d62fbd463982f09765d9e
Ali127Hub <AliMoradi0Business@gmail.com>
2026-05-05 22:22:15 +0330

    style: store theme in localstorage

--- src/features/shared/store/theme.ts
+++ src/features/shared/store/theme.ts
@@ -5,21 +5,31 @@ type theme = 'light' | 'dark';
 interface Theme {
   theme: theme;
   toggle: () => void;
+  setTheme: (mode: theme) => void;
 }
 
-export const useTheme = create<Theme>((set, get) => {
-  const applyTheme = (mode: theme) => {
+export const useTheme = create<Theme>((set) => ({
+  theme: 'light',
+
+  setTheme: (mode) => {
     const root = document.documentElement;
     if (mode === 'dark') root.classList.add('dark');
     else root.classList.remove('dark');
-  };
 
-  return {
-    theme: 'light',
-    toggle: () => {
-      const newTheme = get().theme === 'dark' ? 'light' : 'dark';
-      set({ theme: newTheme });
-      applyTheme(newTheme);
-    },
-  };
-});
+    localStorage.setItem('theme', mode);
+    set({ theme: mode });
+  },
+
+  toggle: () => {
+    set((state) => {
+      const newTheme = state.theme === 'dark' ? 'light' : 'dark';
+      const root = document.documentElement;
+
+      if (newTheme === 'dark') root.classList.add('dark');
+      else root.classList.remove('dark');
+
+      localStorage.setItem('theme', newTheme);
+      return { theme: newTheme };
+    });
+  },
+}));
