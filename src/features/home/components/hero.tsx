56ba53273f8c1be23eff01794c2dade1e8b3c976
Ali127Hub <AliMoradi0Business@gmail.com>
2026-05-05 18:40:10 +0330

    style: hero is responsive

--- src/features/home/components/hero.tsx
+++ src/features/home/components/hero.tsx
@@ -11,16 +11,19 @@ export const Hero: React.FC = () => {
   const isDark = theme === 'dark';
 
   return (
-    <section className="container grid grid-cols-2 gap-8 py-34">
-      <div>
-        <h2 className="font-ernesto mb-4 text-6xl">{hero_date.question}</h2>
-        <div className="space-y-2 leading-6">
+    <section className="container grid grid-cols-2 gap-12 py-8 sm:py-16 md:gap-8 md:py-24 lg:py-34">
+      <div className="col-span-2 md:col-span-1">
+        <h2 className="font-ernesto mb-4 text-4xl sm:text-6xl">
+          {hero_date.question}
+        </h2>
+        <div className="space-y-2 text-sm leading-6 sm:text-base">
           {hero_date.answers.map((el, i) => (
             <p key={`hero_answers_${i}`}>{el}</p>
           ))}
         </div>
       </div>
-      <div className="flex justify-center">
+
+      <div className="col-span-2 flex justify-center md:col-span-1 md:justify-end lg:justify-center">
         <img
           className={clsx('h-2/3 max-h-full rounded-xl drop-shadow-2xl', {
             'drop-shadow-white': isDark,
