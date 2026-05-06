693593d8bf6dfc7366dd6d3d50b1053de8339921
Ali127Hub <AliMoradi0Business@gmail.com>
2026-05-06 08:45:02 +0330

    feat: config aos css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '@/App.css';
+import 'aos/dist/aos.css';
 import { CommonLayout } from '~pages/layout';
 import { Home } from '~pages/home';
 import { NotFound } from './pages/404';
 import { About } from './pages/about';
 import { Products } from './pages/products';
+import { useTheme } from './features/shared/store/theme';
+import React from 'react';
+import AOS from 'aos';
 
 function App() {
+  const setTheme = useTheme((state) => state.setTheme);
+
+  React.useEffect(() => {
+    const savedTheme = localStorage.getItem('theme') as 'dark' | 'light' | null;
+    if (savedTheme) setTheme(savedTheme);
+
+    AOS.init({
+      duration: 800,
+      once: true,
+      easing: 'ease-in-out',
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,18 +1,9 @@
 import { Footer } from '@/features/shared/components/footer';
-import { useTheme } from '@/features/shared/store/theme';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '~features/shared/components/header';
 
 export const CommonLayout: React.FC = () => {
-  const setTheme = useTheme((state) => state.setTheme);
-
-  React.useEffect(() => {
-    const savedTheme = localStorage.getItem('theme') as 'dark' | 'light' | null;
-
-    if (savedTheme) setTheme(savedTheme);
-  }, []);
-
   return (
     <>
       <Header />
